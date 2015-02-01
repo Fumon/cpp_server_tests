2677dcdb2259eb5f48bedfbfa81e0ca776bee674
Jade Bilkey <herself@thefumon.com>
2015-02-01 18:47:48 -0500

    Made server asynchronous

--- main.cpp
+++ main.cpp
@@ -21,33 +21,50 @@ int main() {
 
     // Init acceptor
     tcp::acceptor acceptor(io,
-        tcp::endpoint(tcp::v4(), 9986));
+        tcp::endpoint(tcp::v4(), 9986),
+        tcp::acceptor::reuse_address(true));
+    tcp::socket isocket(io);
 
     // Setup signal
-    std::signal(SIGINT, [&] (int) { exit(0);});
+    std::signal(SIGINT, [] (int) {exit(0);});
 
-    for (;;) {
-      tcp::socket socket(io);
-      acceptor.accept(socket);
+    // Acceptor function
+    std::function<void (void)> do_accept = [&] () {
+      acceptor.async_accept(
+          isocket,
+          [&] (const boost::system::error_code& error) {
+          if(!error) {
+            // Client accepted, read until closed
+            std::cout << "Connection opened from: " << isocket.remote_endpoint().address() << std::endl;
+            std::cout << "Printing incoming data:" << std::endl;
+            for (;;) {
+              boost::array <char, 255> recv_buf;
+              boost::system::error_code err;
 
-      // Client accepted, read until closed
-      std::cout << "Connection opened from: " << socket.remote_endpoint().address() << std::endl;
-      std::cout << "Printing incoming data:" << std::endl;
-      for (;;) {
-        boost::array <char, 255> recv_buf;
-        boost::system::error_code err;
+              std::size_t len = isocket.read_some(boost::asio::buffer(recv_buf),
+                  err);
+              if(err == boost::asio::error::eof) {
+                std::cout << "Connection Closed" << std::endl;
+                // Close the socket
+                isocket.close();
+                break;
+              } else if(err)
+                throw boost::system::system_error(err);
 
-        std::size_t len = socket.read_some(boost::asio::buffer(recv_buf),
-            err);
-        if(err == boost::asio::error::eof) {
-          std::cout << "Connection Closed" << std::endl;
-          break;
-        } else if(err)
-          throw boost::system::system_error(err);
+              std::cout.write(recv_buf.data(), len);
+            }
+            std::cout << "Repriming acceptor" << std::endl;
+            do_accept();
+            std::cout << "primed" << std::endl;
+          } else
+            throw::boost::system::system_error(error);
+        }
+      );
+    };
 
-        std::cout.write(recv_buf.data(), len);
-      }
-    }
+    do_accept();
+
+    io.run();
   } catch (std::exception& e) {
     std::cerr << e.what() << std::endl;
   }
